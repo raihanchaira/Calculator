686691f36c64e3f44421602a2c4b91f194390f98
raihan chaira <raihanchaira21@gmail.com>
2024-02-25 14:21:38 +0700

    main operation

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
+import com.example.calculator.helper.MathOperation
 import java.lang.StringBuilder
 
 
@@ -10,59 +13,78 @@ class MainActivity : AppCompatActivity() {
 
     //variable setup
     var digitOnScreen = StringBuilder()
+    var operator = ' '
+    var leftSide : Double = 0.0
+    var rightSide : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        buttonSetup()
-        functionalButton()
+        setupAll()
 
+    }
+
+    private fun setupAll(){
+        buttonSetup()
+        operationalButton()
+        functionalButton()
     }
 
     private fun buttonSetup(){
         binding.btnOne.setOnClickListener {
             appendDigitOnScreen("1")
+            binding.tvResult.text = "0"
         }
 
         binding.btnTwo.setOnClickListener {
             appendDigitOnScreen("2")
+            binding.tvResult.text = "0"
         }
 
         binding.btnThree.setOnClickListener {
             appendDigitOnScreen("3")
+            binding.tvResult.text = "0"
         }
 
         binding.btnFour.setOnClickListener {
             appendDigitOnScreen("4")
+            binding.tvResult.text = "0"
         }
 
         binding.btnFive.setOnClickListener {
             appendDigitOnScreen("5")
+            binding.tvResult.text = "0"
         }
 
         binding.btnSix.setOnClickListener {
             appendDigitOnScreen("6")
+            binding.tvResult.text = "0"
         }
 
         binding.btnSeven.setOnClickListener {
             appendDigitOnScreen("7")
+            binding.tvResult.text = "0"
         }
 
         binding.btnEight.setOnClickListener {
             appendDigitOnScreen("8")
+            binding.tvResult.text = "0"
         }
 
         binding.btnNine.setOnClickListener {
             appendDigitOnScreen("9")
+            binding.tvResult.text = "0"
         }
 
         binding.btnZero.setOnClickListener {
             appendDigitOnScreen("0")
+            binding.tvResult.text = "0"
         }
 
         binding.btnComa.setOnClickListener {
             appendDigitOnScreen(",")
+            binding.tvResult.text = "0"
         }
     }
 
@@ -78,13 +100,82 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnClearAll.setOnClickListener {
             digitOnScreen.clear()
+            binding.tvOperation.text = ""
+            binding.tvResult.text = ""
         }
+
+        binding.btnEqual.setOnClickListener {
+            mainOperation()
+        }
+
+
     }
 
     private fun clearPerDigit(){
         var length = digitOnScreen.length
 
-        digitOnScreen.deleteCharAt(length - 1)
-        binding.tvOperation.text = digitOnScreen.toString()
+        if (length <= 0){
+            Toast.makeText(this, "no digit", Toast.LENGTH_SHORT).show()
+        }else{
+            digitOnScreen.deleteCharAt(length - 1)
+            binding.tvOperation.text = digitOnScreen.toString()
+        }
+    }
+
+    private fun selectOperation(sign : Char){
+        operator = sign
+        leftSide = digitOnScreen.toString().toDouble()
+        digitOnScreen.clear()
+        binding.tvResult.text = "0"
+    }
+
+    private fun operationalButton(){
+        binding.btnPlus.setOnClickListener {
+             selectOperation('A')
+        }
+
+        binding.btnMinus.setOnClickListener {
+             selectOperation('B')
+        }
+
+        binding.btnMultiple.setOnClickListener {
+             selectOperation('C')
+        }
+
+        binding.btnDivided.setOnClickListener {
+             selectOperation('D')
+        }
+
+    }
+
+    private fun mainOperation(){
+        rightSide = digitOnScreen.toString().toDouble()
+        digitOnScreen.clear()
+
+        when (operator) {
+            'A' -> {
+                val sum = MathOperation.addition(leftSide, rightSide)
+                binding.tvResult.text = sum.toString()
+                digitOnScreen.append(sum)
+            }
+
+            'B' -> {
+                val subs = MathOperation.addition(leftSide, rightSide)
+                binding.tvResult.text = subs.toString()
+                digitOnScreen.append(subs)
+            }
+
+            'C' -> {
+                val mulp = MathOperation.addition(leftSide, rightSide)
+                binding.tvResult.text = mulp.toString()
+                digitOnScreen.append(mulp)
+            }
+
+            'D' -> {
+                val div = MathOperation.addition(leftSide, rightSide)
+                binding.tvResult.text = div.toString()
+                digitOnScreen.append(div)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/calculator/helper/MathOperation.kt
@@ -0,0 +1,24 @@
+package com.example.calculator.helper
+
+/** Raihan Chaira on 2/25/2024
+ * raihanchaira21@gmail.com
+ */
+class MathOperation {
+    companion object{
+        fun addition(rightSide : Double, leftSide : Double) : Double{
+            return rightSide + leftSide
+        }
+
+        fun substraction(rightSide : Double, leftSide : Double) : Double{
+            return rightSide - leftSide
+        }
+
+        fun multiple(rightSide : Double, leftSide : Double) : Double{
+            return rightSide * leftSide
+        }
+
+        fun divider(rightSide : Double, leftSide : Double) : Double{
+            return rightSide / leftSide
+        }
+    }
+}
